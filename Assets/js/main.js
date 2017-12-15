b389d5524245d624ce3352be99722bedf30c59d7
drj613 <drj613@gmail.com>
2017-12-14 19:53:00 -0800

    Clickable map, starting animation

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,6 +1,21 @@
-var $ = require('jquery-browserify');
-require('jquery-mousewheel')($);
-
 $(document).ready(function(){
     
-})
+    var vertCarriage = new Image();
+    vertCarriage.src = '../img/vertCarriage.png';
+
+    function sprite(){
+        var that = {};
+
+        that.context = options.context;
+        that.width = options.width;
+        that.height = options.height;
+        that.image = options.image;
+
+        return that;
+    }
+
+    var $carriageCanvas = $("animTest");
+    $carriageCanvas.width = 
+
+    
+});
