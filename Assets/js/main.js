abb0b89b6cef4ebfd2d2208a813443d2625c35a5
drj613 <drj613@gmail.com>
2017-12-16 13:56:30 -0800

    Moved animation to its own file

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -6,78 +6,24 @@ $(document).ready(function () {
         return false;
     });
 
+    // Init sprite sheets for carriage
     var leftCarriage = new Image();
     leftCarriage.src = './Assets/img/horizCarriage-transparent.png';
 
     var rightCarriage = new Image();
     rightCarriage.src = './Assets/img/horizCarriageRt-transparent.png';
 
-    function sprite(options){
-        var that = {},
-            frameIndex = 0,
-            tickCount = 0,
-            ticksPerFrame = options.ticksPerFrame || 0;
-            numberOfFrames = options.numberOfFrames || 1;
+    //Coordinates for cities on areamap
+    var $castleBlackPos = $("#castleBlack").attr('coords').split(',');
+    var $winterfellPos = $("#winterfell").attr('coords').split(',');
+    var $theTwinsPos = $("#theTwins").attr('coords').split(',');
+    var $pykePos = $("#pyke").attr('coords').split(',');
+    var $theEyriePos = $("#theEyrie").attr('coords').split(',');
+    var $harrenhalPos = $("#harrenhal").attr('coords').split(',');
+    var $dragonstonePos = $("#dragonstone").attr('coords').split(',');
+    var $kingsLandingPos = $("#kingsLanding").attr('coords').split(',');
+    var $firstMenPos = $("#firstMen").attr('coords').split(',');
+    var $pentosPos = $("#pentos").attr('coords').split(',');  
 
-        that.context = options.context;
-        that.width = options.width;
-        that.height = options.height;
-        that.image = options.image;
-        that.loop = options.loop;
-        that.render = function () {
-            that.context.clearRect(0, 0, that.width, that.height);
-            // context.drawImage(img, sourcex, sourcey, sourcewidth, sourceheight, destinationx, destinationy, destinationwidth, destinationheight)
-            that.context.drawImage(
-                that.image,
-                frameIndex * that.width / numberOfFrames,
-                0,
-                that.width / numberOfFrames,
-                that.height,
-                0,
-                0,
-                that.width / numberOfFrames,
-                that.height);
-        };
-        that.update = function () {
-            tickCount++;
-
-            if(tickCount > ticksPerFrame) {
-                tickCount = 0;
-
-                if (frameIndex < numberOfFrames - 1) {
-                    frameIndex++;
-                } else if (that.loop) {
-                    frameIndex = 0;
-                }
-            }
-        }
-
-        return that;
-    }
-
-    var canvas = document.getElementById("animTest");
-    canvas.width = 2452;
-    canvas.height = 3064;
-
-    var hrzMove = sprite({
-        context: canvas.getContext("2d"),
-        width: 920,
-        height: 90,
-        image: leftCarriage,
-        numberOfFrames: 5,
-        ticksPerFrame: 7,
-        loop: true
-    });
-
-
-    leftCarriage.onload = hrzMove.render;
-
-    function gameLoop(){
-        window.requestAnimationFrame(gameLoop);
-
-        hrzMove.update();
-        hrzMove.render();
-    }
-
-    leftCarriage.addEventListener('load', gameLoop);
+    
 });
