be284893d578ccbf9a1a5c2c6bba46fe1c374527
drj613 <drj613@gmail.com>
2017-12-28 20:23:19 -0800

    fiddling with getting animation to move

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -2,48 +2,46 @@
 // 2) If/else of currentPos.x - destinationPos.x to decide which avatar to use
 // 3) incrementally change position of sprite until reaching destination (JQUERY .ANIMATE() move destPos.x - currPos.X, slow)
 // 4) set "choke points" on map, where canvas has to go to avoid jankiness going through bays and whatnot
-
+// 5) functions to skip and stop animation
 $(document).ready(function () {
-    
-    
-        //Coordinates for cities on areamap
-        var $castleBlackPos = $("#castleBlack").attr('coords').split(',');
-        var $winterfellPos = $("#winterfell").attr('coords').split(',');
-        var $theTwinsPos = $("#theTwins").attr('coords').split(',');
-        var $pykePos = $("#pyke").attr('coords').split(',');
-        var $theEyriePos = $("#theEyrie").attr('coords').split(',');
-        var $harrenhalPos = $("#harrenhal").attr('coords').split(',');
-        var $dragonstonePos = $("#dragonstone").attr('coords').split(',');
-        var $kingsLandingPos = $("#kingsLanding").attr('coords').split(',');
-        var $pentosPos = $("#pentos").attr('coords').split(',');  
 
-        // Init sprite sheets for carriage
-        var leftCarriage = new Image();
-        leftCarriage.src = './assets/img/horizCarriage-transparent.png';
-    
-        var rightCarriage = new Image();
-        rightCarriage.src = './assets/img/horizCarriageRt-transparent.png';
-    // Sprite constructor
-        function sprite(options){
-            var that = {},
-                frameIndex = 0,
-                tickCount = 0,
-                ticksPerFrame = options.ticksPerFrame || 0;
-                numberOfFrames = options.numberOfFrames || 1;
-    
-            that.context = options.context;
-            that.width = options.width;
-            that.height = options.height;
-            that.image = options.image;
-            that.loop = options.loop;
-    
-        // Initial sprite rendering
-        that.render = function () {
-            that.context.clearRect(0, 0, that.width, that.height);
-            if (that.initial){
-                that.initial = false;
-    
-            }
+
+    //Coordinates for cities on areamap
+    var $castleBlackPos = $("#castleBlack").attr('coords').split(',');
+    var $winterfellPos = $("#winterfell").attr('coords').split(',');
+    var $theTwinsPos = $("#theTwins").attr('coords').split(',');
+    var $pykePos = $("#pyke").attr('coords').split(',');
+    var $theEyriePos = $("#theEyrie").attr('coords').split(',');
+    var $harrenhalPos = $("#harrenhal").attr('coords').split(',');
+    var $dragonstonePos = $("#dragonstone").attr('coords').split(',');
+    var $kingsLandingPos = $("#kingsLanding").attr('coords').split(',');
+    var $pentosPos = $("#pentos").attr('coords').split(',');
+
+    // Init sprite sheets for carriage
+    var leftCarriage = new Image();
+    leftCarriage.src = './Assets/img/horizCarriage-transparent.png';
+    leftCarriage.id = 'leftCarriage';
+
+    var rightCarriage = new Image();
+    rightCarriage.src = './Assets/img/horizCarriageRt-transparent.png';
+    rightCarriage.id = 'rightCarriage';
+
+    function sprite(options) {
+        var that = {},
+            frameIndex = 0,
+            tickCount = 0,
+            ticksPerFrame = options.ticksPerFrame || 0;
+        numberOfFrames = options.numberOfFrames || 1;
+
+        that.context = options.context;
+        that.width = options.width;
+        that.height = options.height;
+        that.image = options.image;
+        that.loop = options.loop;
+
+        // Sprite rendering
+        that.render = function (x, y) {
+            that.context.clearRect(x, y, that.width, that.height);
             // context.drawImage(img, sourcex, sourcey, sourcewidth, sourceheight, destinationx, destinationy, destinationwidth, destinationheight)
             that.context.drawImage(
                 that.image,
@@ -51,18 +49,18 @@ $(document).ready(function () {
                 0,
                 that.width / numberOfFrames,
                 that.height,
-                0,
-                0,
+                x,
+                y,
                 that.width / numberOfFrames,
                 that.height);
         };
-        // Updating sprite frames
+        // Updating sprite frame
         that.update = function () {
             tickCount++;
-    
-            if(tickCount > ticksPerFrame) {
+
+            if (tickCount > ticksPerFrame) {
                 tickCount = 0;
-    
+
                 if (frameIndex < numberOfFrames - 1) {
                     frameIndex++;
                 } else if (that.loop) {
@@ -70,49 +68,78 @@ $(document).ready(function () {
                 }
             }
         }
-    
+
         return that;
     }
-    
+
     var canvas = document.querySelector("#animTest");
-    var ctx = canvas.getContext("2d");
     canvas.width = 2452;
     canvas.height = 3064;
-    
+
     var hrzMoveLeft = sprite({
         context: canvas.getContext("2d"),
         width: 920,
         height: 90,
         image: leftCarriage,
         numberOfFrames: 5,
-        ticksPerFrame: 7,
+        ticksPerFrame: 10,
         initial: true,
         loop: true
     });
-    
+
     var hrzMoveRight = sprite({
         context: canvas.getContext("2d"),
         width: 920,
         height: 90,
         image: rightCarriage,
         numberOfFrames: 5,
-        ticksPerFrame: 7,
+        ticksPerFrame: 10,
         loop: true
     });
-    
+
     var canvas = document.getElementById("animTest");
-    
-    window.onload = ctx.drawImage(leftCarriage, 1000, 1000);
-    // leftCarriage.onload = hrzMoveLeft.render;
-    // rightCarriage.onload = hrzMoveRight.render;
-    
-    function gameLoop(){
+
+    var currentPosition = {
+        x: $kingsLandingPos[0],
+        y: $kingsLandingPos[1]
+    };
+
+    var spriteToUse = hrzMoveLeft;
+
+    function gameLoop() {
         window.requestAnimationFrame(gameLoop);
-    
-        var spriteToUse = hrzMoveLeft;
+
+        // Trigger movement of sprite
+        $(".clickable").on("click", function () {
+            var destination = $(this).attr("coords").split(",");
+            var modalId = "#" + $(this).attr("id") + "modal";
+
+            setInterval(move(spriteToUse, destination, modalId), 50);
+        });
+
         spriteToUse.update();
-        spriteToUse.render();
-        }
-    
+        spriteToUse.render(currentPosition.x, currentPosition.y);
+    }
+
     leftCarriage.addEventListener('load', gameLoop);
-    });
+
+    // Function to move sprite to a specific destination (input as an array of coordinates)
+    function move(sprite, destination, modalId) {
+        // Create ID string to use in jquery
+        var spriteId = "#" + spriteToUse.id;
+        var xDiff = currentPosition.x - destination[0];
+        var yDiff = currentPosition.y - destination[1];;
+        $(spriteId).animate({
+            top: yDiff,
+            left: xDiff
+        }, 2000, function () {
+            $(modalId).modal("show");
+        });
+    }
+
+
+
+
+
+
+});
