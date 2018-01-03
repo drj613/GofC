62ae5ea651ccd9512dcc9d6650083def1a8f7818
Victoria Smith <torismith@Victorias-MacBook-Pro-3.local>
2018-01-02 20:38:32 -0800

    splash screen html, css, js

---
+++ public/assets/js/splash.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("#splash-username").hide();
+    });
+
+$(document).ready(function(){
+    $("#agree").click(function(){
+        $("#splash-username").toggle();
+    });
+});
